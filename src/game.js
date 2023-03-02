81a9796d5eda265f7ff6c987150a9d38ac6263d4
flyeers <juliafluper@gmail.com>
2023-03-02 15:38:22 +0100

    dia uno

---
+++ src/game.js
@@ -0,0 +1,53 @@
+import FirstLevel from './scenes/firstLevel.js';
+import Title from './scenes/title.js'
+/**
+ * Inicio del juego en Phaser. Creamos el archivo de configuración del juego y creamos
+ * la clase Game de Phaser, encargada de crear e iniciar el juego.
+ * Doc: https://photonstorm.github.io/phaser3-docs/Phaser.Types.Core.html#.GameConfig
+ */
+let config = {
+    type: Phaser.AUTO,
+    parent: 'juego',
+    // type: Phaser.CANVAS,
+    // canvas: document.getElementById("juego"),
+    
+    width:  656,
+    height: 376,
+    //height: 500,
+
+    pixelArt: true,
+	scale: {
+		autoCenter: Phaser.Scale.CENTER_HORIZONTALLY,
+		// Configuramos phaser para que se adapte al tamaño de pantalla donde ejecutadmos
+		// con un mínimo y un máximo de tamaño
+		mode: Phaser.Scale.FIT,
+		min: {
+            width: 328,
+            height: 188
+        },
+		max: {
+            width: 1312,
+            height: 752
+        },
+		zoom: 1
+    },
+    scene: [Title, FirstLevel],
+    physics: { 
+        default: 'arcade', 
+        arcade: { 
+            //gravity: { y: 200 }, 
+            debug: true 
+        },
+        checkCollision: {
+            up: true,
+            down: true,
+            left: true,
+            right: true
+        }
+    },
+    title: "Prueba de concepto",
+    version: "1.0.0",
+    transparent: false
+};
+
+new Phaser.Game(config);

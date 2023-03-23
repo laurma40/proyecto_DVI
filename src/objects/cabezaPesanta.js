584e6f5cffe564f814b287804f7237a7c09d2f77
laurma40 <98830222+laurma40@users.noreply.github.com>
2023-03-23 14:05:52 +0100

    Update cabezaPesanta.js

--- src/objects/cabezaPesanta.js
+++ src/objects/cabezaPesanta.js
@@ -1,69 +1,67 @@
 export default class CabezaPesanta extends Phaser.GameObjects.Sprite {
-	constructor(scene, x, y, colliderGroup) {
+	constructor(scene, x, y, path) {
 		super(scene, x, y, 'cabezaPesanta');
-		this.setScale(0.2);
-
+		this.setScale(0.1);
+		this.path = path;
+		this.t = 0;
+		this.speed = 100;
+		//this.easing = null;
+		this.x = x;
+		this.y = y;
 		this.scene.add.existing(this); //Añadimos el caballero a la escena
 
 		// Creamos las animaciones de cabeza de pesanta
 		this.scene.anims.create({
-			key: 'front',
+			key: 'frontP',
 			frames: scene.anims.generateFrameNumbers('cabezaPesanta', {start:1, end:4}),
 			frameRate: 5,
 			repeat: -1
 		});
 		this.scene.anims.create({
-			key: 'back',
+			key: 'backP',
 			frames: scene.anims.generateFrameNumbers('cabezaPesanta', {start:6, end:9}),
 			frameRate: 5,
 			repeat: -1
 		});
 		this.scene.anims.create({
-			key: 'right',
+			key: 'rightP',
 			frames: scene.anims.generateFrameNumbers('cabezaPesanta', {start:11, end:14}),
 			frameRate: 5,
 			repeat: -1
 		});
 		this.scene.anims.create({
-			key: 'left',
+			key: 'leftP',
 			frames: scene.anims.generateFrameNumbers('cabezaPesanta', {start:16, end:19}),
 			frameRate: 5,
 			repeat: -1
 		});
 		///
 		this.scene.anims.create({
-			key: 'standFront',
+			key: 'standFrontP',
 			frames: scene.anims.generateFrameNumbers('cabezaPesanta', {start:0, end:1}),
 			frameRate: 5,
 			repeat: -1
 		});
 		this.scene.anims.create({
-			key: 'standBack',
+			key: 'standBackP',
 			frames: scene.anims.generateFrameNumbers('cabezaPesanta', {start:5, end:6}),
 			frameRate: 5,
 			repeat: -1
 		});
 		this.scene.anims.create({
-			key: 'standRight',
+			key: 'standRightP',
 			frames: scene.anims.generateFrameNumbers('cabezaPesanta', {start:10, end:11}),
 			frameRate: 5,
 			repeat: -1
 		});
 		this.scene.anims.create({
-			key: 'standLeft',
+			key: 'standLeftP',
 			frames: scene.anims.generateFrameNumbers('cabezaPesanta', {start:15, end:16}),
 			frameRate: 5,
 			repeat: -1
 		});
 
-		this.play('front');
-
-		// Seteamos las teclas para mover al personaje
-		this.wKey = this.scene.input.keyboard.addKey('W'); //saltar
-		this.aKey = this.scene.input.keyboard.addKey('A'); //izquierda
-		this.sKey = this.scene.input.keyboard.addKey('S'); //parar animación
-		this.dKey = this.scene.input.keyboard.addKey('D'); //derecha
-		
+		this.play('frontP');
 	}
 	/**
 	 * Bucle principal del personaje, actualizamos su posición y ejecutamos acciones según el Input
@@ -72,50 +70,50 @@ export default class CabezaPesanta extends Phaser.GameObjects.Sprite {
 	 */
 	preUpdate(t, dt) {
 		// Es muy imporante llamar al preUpdate del padre (Sprite), sino no se ejecutará la animación
-		super.preUpdate(t, dt);
+		super.preUpdate(t, dt);	
 
-		// Mientras pulsemos la tecla 'A' movemos el personaje en la X
-		if(this.aKey.isDown){
-			if(this.anims.currentAnim.key !== 'left')
-				this.play('left');
-			this.x -= 5*dt/60;
-		}
+		if (this.path) {
+			this.t += dt*0.001*this.speed;
+			this.t = Phaser.Math.Clamp(this.t, 0, 1);
 
-		else if(this.aKey.isUp && this.anims.currentAnim.key === 'left') {
-			this.play('standLeft');
-		}
+			console.log("coords");
 
-		// Mientras pulsemos la tecla 'D' movemos el personaje en la X
-		else if(this.dKey.isDown){
-			if(this.anims.currentAnim.key !== 'right')
-				this.play('right');
-			this.x += 5*dt/60;
-		}
-
-		else if(this.dKey.isUp && this.anims.currentAnim.key === 'right') {
-			this.play('standRight');
-		}
-		
-		// Mientras pulsemos la tecla 'W' movemos el personaje en la Y
-		else if(this.wKey.isDown){
-			if(this.anims.currentAnim.key !== 'back')
-				this.play('back');
-			this.y -= 5*dt/60;
-		}
-
-		else if(this.wKey.isUp && this.anims.currentAnim.key === 'back') {
-			this.play('standBack');
-		}
-
-		// Mientras pulsemos la tecla 'S' movemos el personaje en la Y
-		else if(this.sKey.isDown){
-			if(this.anims.currentAnim.key !== 'front')
-				this.play('front');
-			this.y += 5*dt/60;
-		}
-
-		else if(this.sKey.isUp && this.anims.currentAnim.key === 'front') {
-			this.play('standFront');
+			var position = this.path.getPoint(this.t);
+			console.log(position);
+			// Check if sprite's position has incremented or decremented since the last update
+			if (position.x > this.x) {
+			  	this.anims.play('rightP');
+				//this.body.setVelocityX(this.speed*dt/60);
+			} 
+			else if (position.x < this.x) {
+			  	this.anims.play('leftP');
+				//this.body.setVelocityX(-this.speed*dt/60);
+			} 
+			else if (position.y > this.y){
+				this.anims.play('frontP');
+				//this.body.setVelocityY(-this.speed*dt/60);	
+			}
+			else if (position.y < this.y){
+				this.anims.play('backP');
+				//this.body.setVelocityY(this.speed*dt/60);	
+			}
+			else {
+				if (this.anims.currentAnim.key === 'rightP')
+					this.anims.play('standRightP');
+				else if (this.anims.currentAnim.key === 'leftP')
+					this.anims.play('standLeftP');
+				else if (this.anims.currentAnim.key === 'frontP')
+					this.anims.play('standFrontP');
+				else if (this.anims.currentAnim.key === 'backP')
+					this.anims.play('standBackP');
+			}
+			this.x = position.x;
+			this.y = position.y;
+	  
+			this.setPosition(position.x, position.y);
+			if (t === 1) {
+			  this.path = null;
+			}	  
 		}
 	}
 }
